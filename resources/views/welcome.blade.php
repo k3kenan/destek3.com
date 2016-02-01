12885e744db7b669804a7c33b6c27d55e2ff979b
k3kenan <kenankaankurt@gmail.com>
2016-02-01 23:47:53 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>Test</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Destek3 Coming Soon</div>
+                <div class="title">DesteK3</div>
             </div>
         </div>
     </body>

eb4fa78d0f1aa7ac3ea9c5d6c36f467903789f50
k3kenan <kenankaankurt@gmail.com>
2016-02-05 15:50:31 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -54,10 +54,10 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'destek3'),
-            'username'  => env('DB_USERNAME', 'test'),
-            'password'  => env('DB_PASSWORD', 'qwerty'),
+            'host'      => env('DB_HOST'),
+            'database'  => env('DB_DATABASE'),
+            'username'  => env('DB_USERNAME'),
+            'password'  => env('DB_PASSWORD'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
